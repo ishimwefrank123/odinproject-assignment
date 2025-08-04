111f35b76775830aac7fb24a693d0609caf29a39
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2025-08-04 14:01:42 +0200

    what is missing for deploy

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 
 //Server
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 app.listen(PORT, ()=>{
   console.log(`Server is running on http://localhost:${PORT}`)
 })
